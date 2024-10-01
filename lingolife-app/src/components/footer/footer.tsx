2026ac98aea20fafc5dfc0ecade811e7f624fdb5
davicalebe <ayykarumba@gmail.com>
2024-10-01 16:11:01 -0300

    test

---
+++ lingolife-app/src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+export default function Home(){
+  return (
+    <>
+      <footer>
+        <h1>LingoLife © Copyright 2024. All Rights Reserved. </h1>
+      </footer>
+    </>
+  )
+}
---
+++ lingolife-app/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/footer/footer.tsx';
+
+function Home() {
+
+  return (
+    <>
+      <h1>Bem vindo ao LingoLife</h1>
+      <h2>Conectando Mentes e Linguagens!</h2>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
