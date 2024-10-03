f184991685be0e03cdfbf8eb9d8539805ba14a2a
DaviCalebe <ayykarumba@gmail.com>
2024-10-03 15:08:20 -0300

    feat: description on home

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+export default function Home(){
+  return (
+    <>
+      <footer>
+        <p>LingoLife © Copyright 2024. All Rights Reserved. </p>
+      </footer>
+    </>
+  )
+}
