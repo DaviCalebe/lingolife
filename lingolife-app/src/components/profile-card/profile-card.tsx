a5c77ad195520cdbf588a9d80f026e016bb79ab5
DuMilo <milomoreirac@gmail.com>
2024-10-17 14:54:34 -0300

    fix: profile-card files

--- lingolife-app/src/components/profile-card/profile-card.tsx
+++ lingolife-app/src/components/profile-card/profile-card.tsx
@@ -1,31 +1,50 @@
 import './profile-card.scss'
 
 interface IProfileCard {
-    name: string,
-    image?: string,
-    language: string,
-    about?: string
+  name: string,
+  image?: string,
+  language: string,
+  about?: string,
+  buttonText?: string
 }
 
-const ProfileCard = ( {name='', image='', language='', about='' } :IProfileCard) => {
-    return(
-        <div className='profile-card'>
-            <div className="user-header">
-                <div className="grid-centralize">
-                    <div className="under-box"></div>
-                    <img src={image} alt="profile-pic" />
+const ProfileCard = ({
+  name = '',
+  image = '',
+  language = '',
+  about = '',
+  buttonText = 'VISITAR O PERFIL'
+}: IProfileCard) => {
+  return (
+    <div className="profile-card">
+      <div className="user-header">
+        <div className="grid-centralize">
+            {image ? (
+                <img src={image} alt="profile-pic" />
+            ) : (
+                <div className="over-box">
+                    <p className='no-image'>Sem imagem</p>
                 </div>
-                <div className="user-info">
-                    <h1>{name}</h1>
-                    <h2>{language}</h2>
-                </div>
-            </div>
-            <div className="user-description">
-                <p>{about}</p>
-            </div>
-            <button className='dark-blue-button'>VISITAR O PERFIL</button>
+                
+                
+            )}
+          <div className="under-box"></div>
         </div>
-    )
+        <div className="user-info">
+          <h1>{name}</h1>
+          <h2>{language}</h2>
+        </div>
+      </div>
+      {about ? (
+        <div className="user-description">
+          <p>{about}</p>
+        </div>
+      ) : (
+        <p className="no-about">Sem descrição</p>
+      )}
+      <button className="dark-blue-button">{buttonText}</button>
+    </div>
+  )
 }
 
 export default ProfileCard
