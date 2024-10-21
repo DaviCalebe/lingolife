db08c65d1603c0f7a19af31f9e39426a5d922c3a
davicalebe <ayykarumba@gmail.com>
2024-10-21 13:32:23 -0300

    fix: loading component

--- lingolife-app/src/components/loading/loading.tsx
+++ lingolife-app/src/components/loading/loading.tsx
@@ -1,6 +1,9 @@
+import './loading.scss'
+
 const Loading = () => {
     return (
       <div className="loading-container">
+        <h1>Carregando...</h1>
         <div className="loading-spinner"></div>
       </div>
     );
