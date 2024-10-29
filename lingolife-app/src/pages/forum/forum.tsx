0e6ca8dd0a720a2133c6ce398aeef6c7133df569
davicalebe <ayykarumba@gmail.com>
2024-10-29 12:53:50 -0300

    fix: forum page

--- lingolife-app/src/pages/forum/forum.tsx
+++ lingolife-app/src/pages/forum/forum.tsx
@@ -19,6 +19,7 @@ const Forum = () => {
         const loadPublications = async () => {
             try {
                 const data = await fetchPublications();
+                console.log(data)
                 setPublications(data);
             } catch (error) {
                 setError(error instanceof Error ? error.message : 'Erro desconhecido');
@@ -42,9 +43,8 @@ const Forum = () => {
                 {error ? (
                     <p>{error}</p>
                 ) : (
-                    publications.map((pub, index) => (
-                        <ForumCard 
-                            key={index}
+                    publications.map((pub) => (
+                        <ForumCard
                             name="teste" // Assuming name is part of the publication data
                             buttonText="Visitar o perfil" // You might want to adjust this based on your data
                             title={pub.title}
@@ -52,36 +52,8 @@ const Forum = () => {
                             fileSrc={pub.fileSrc}
                             language='portuguese'
                         />
-                    ))
+                        ))
                 )}
-{/*                 <ForumCard 
-                    name="Carlos Pereira"
-                    buttonText="Visitar o perfil"
-                    language="Português"
-                    title="Melhores práticas em desenvolvimento web"
-                    content="Explore as melhores práticas para criar aplicações web escaláveis e de fácil manutenção."
-                />
-                <ForumCard 
-                    name="Ana Costa"
-                    buttonText="Visitar o perfil"
-                    language="Espanhol"
-                    title="Entendiendo el JSX"
-                    content="JSX es una extensión de sintaxis para JavaScript que permite escribir HTML dentro del JavaScript."
-                />
-                <ForumCard 
-                    name="Lucas Almeida"
-                    buttonText="Visitar o perfil"
-                    language="Português"
-                    title="Gerenciamento de estado com Redux"
-                    content="Aprenda como usar o Redux para gerenciar o estado da sua aplicação React."
-                />
-                <ForumCard 
-                    name="Fernanda Santos"
-                    buttonText="Visitar o perfil"
-                    language="Inglês"
-                    title="Styling Components with SASS"
-                    content="Discover how to use SASS to style your components efficiently."
-                /> */}
 
             </div>
         </main>
