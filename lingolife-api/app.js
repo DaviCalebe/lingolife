116a1f91262a6283da904ab41073f2301710f1eb
pedrofelipemoreira <pfelope@gmail.com>
2024-11-12 22:47:03 -0300

    adição do login ao backend

--- lingolife-api/app.js
+++ lingolife-api/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import conn from './db/conn.js' 
-import ForumRouter from './routes/Forum.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+//import routes
+
+import ForumRouter from './routes/Forum.js'
+import UserRoutes from './routes/UserRoutes.js';
 
 //Db Connection
 conn();
@@ -16,10 +20,15 @@ app.use(cors());
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+//solve cors
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 app.use(express.static('public'));
 
 // Routes
 app.use("/forum",ForumRouter);
+app.use('/users', UserRoutes)
+
 
 app.listen(5000, function(){
     console.log("Servidor Online!!");
---
+++ lingolife-api/routes/UserRoutes.js
@@ -0,0 +1,26 @@
+import  express  from 'express';
+const router = express.Router();
+
+import UserController from '../controllers/UserController.js';
+
+//midleware
+import verifyToken from "../helpers/verify-token.js"
+import imageUpload from '../helpers/image-uploads.js';
+
+router.route('/register')
+.post((req, res) => UserController.register(req, res));
+
+router.route('/login')
+.post((req, res) => UserController.login(req, res));
+
+router.route('/checkuser')
+.get((req, res) => UserController.checkUser(req, res));
+
+router.route('/:id')
+.get((req, res) => UserController.getUserById(req, res));
+
+router.route('/edit/:id')
+.patch(verifyToken, imageUpload.single('image'),(req, res) =>  UserController.editUser(req, res));
+
+
+export default router; 
