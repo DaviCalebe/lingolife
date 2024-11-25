85e9bef235f68ef73f29191de10d5b6725018862
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-11-25 03:48:48 -0300

    cors

--- lingolife-api/app.js
+++ lingolife-api/app.js
@@ -1,39 +1,37 @@
 import express from 'express';
 import cors from 'cors';
-import conn from './db/conn.js' 
+import conn from './db/conn.js'; 
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-//import routes
-
-import ForumRouter from './routes/Forum.js'
+// Import routes
+import ForumRouter from './routes/Forum.js';
 import UserRoutes from './routes/UserRoutes.js';
 import chatRouter from './routes/chatRoute.js';
-import messageRouter from './routes/messageRoute.js'
+import messageRouter from './routes/messageRoute.js';
 
-//Db Connection
+// DB Connection
 conn();
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors()); 
-app.use(express.json())
+// Middleware para CORS - permite todas as origens
+app.use(cors({ credentials: true, origin: '*' }));
+
+// Middleware para JSON e arquivos estáticos
+app.use(express.json());
 app.use('/public/filesForum', express.static(path.join(__dirname, 'uploads')));
-
-//solve cors
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-
 app.use(express.static('public'));
 
 // Routes
-app.use("/forum",ForumRouter);
-app.use('/users', UserRoutes)
-app.use("/chats", chatRouter)
-app.use("/messages",messageRouter)
+app.use('/forum', ForumRouter);
+app.use('/users', UserRoutes);
+app.use('/chats', chatRouter);
+app.use('/messages', messageRouter);
 
-
-app.listen(5000, function(){
-    console.log("Servidor Online!!");
+// Start server
+app.listen(5000, function () {
+  console.log('Servidor Online!!');
 });
